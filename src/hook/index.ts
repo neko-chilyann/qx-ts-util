7e740385113075ae7eb0eb65af481b5408422b99
chitanda <1423103@qq.com>
2021-03-08 15:35:18 +0800

    补充导出

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,2 +1,3 @@
+export { SyncSeriesHook } from './sync-series-hook/sync-series-hook';
 export { AsyncSeriesHook } from './async-series-hook/async-series-hook';
 export { IHookCallItem } from './interface/i-hook-call-item';
